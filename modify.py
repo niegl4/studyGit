e628c141f02dc6175e9f9e96d4275e1c10895f5e
sz28_nieguanglin <1097723907@qq.com>
2020-06-24 00:38:59 +0800

    继续造冲突

--- modify.py
+++ modify.py
@@ -9,17 +9,6 @@ print("hello python")
 print("hello ")
 
 #同事新写的代码1
-#2
-#3
-#4
 
 #我在他的基础上新写的代码1
-#2
-#3
-#4
-#print("hello python")
-print("hello python")
-print("hello python")
-print("hello python")
-print("hello ")
 
