06cbf91e05fc797f646c10c4517de878c2fc967a
sz28_nieguanglin <1097723907@qq.com>
2020-06-23 22:54:21 +0800

    继续造成冲突

--- modify.py
+++ modify.py
@@ -1,4 +1,9 @@
-# print("hello world")
 print("hello world")
-print("hello")
-# print("hello python")
+print("hello 123")
+
+#
+print("hello python")
+print("hello python")
+print("hello python")
+print("hello python")
+print("hello python")
