400b84195b96b65ccbf2ff06536edc81ac9799c2
sz28_nieguanglin <1097723907@qq.com>
2020-07-04 10:42:27 +0800

    bbbb

--- modify.py
+++ modify.py
@@ -2,12 +2,6 @@ print("hello world")
 print("hello 123")
 print("hello 123")
 
-#
-print("hello python")
-print("hello python")
-print("hello python")
-print("hello python")
-print("hello ")
 
 #同事新写的代码1
 #同事新写的代码2
